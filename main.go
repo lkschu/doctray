06f1b37c29d69fed905c2ffc36125f02eb897815
Lukas K Schumann <lukas_kilian.schumann@stud-mail.uni-wuerzburg.de>
2025-05-13 00:14:22 +0200

    introducing htmx

--- main.go
+++ main.go
@@ -2,7 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"slices"
+	"time"
+    "strconv"
+
+	"encoding/json"
 
 	oidcauth "github.com/TJM/gin-gonic-oidcauth"
 	"github.com/gin-contrib/sessions"
@@ -25,6 +32,78 @@ func ExampleConfigAuthentik() (c *oidcauth.Config) {
 	return
 }
 
+type test_data struct {
+    Num int             `json:"num"`
+    Key string          `json:"key"`
+}
+
+type meta_info struct {
+    selected bool
+}
+
+type doc struct {
+    Date_added string   `json:"date"`
+    Title string        `json:"title"`
+    Description string  `json:"desc"`
+    Url string          `json:"url"`
+    Image string        `json:"image"`
+    Tags []string       `json:"tags"`
+    Meta meta_info      `json:"-"`
+}
+func (d doc) String() string {
+    b,err := json.Marshal(d)
+    if err != nil {
+        fmt.Println(err)
+        panic(err)
+    }
+    return string(b)
+}
+func (doc) init_data() []doc {
+    doc1 := doc{Date_added:"2025-05-12T18:25", Title:"Coca-Cola braised carnitas | Homesick Texan",
+    Description:"Coca-Cola and milk, along with warm flavors such as cinnamon, allspice, and chipotle give these carnitas a slight hint of bacon, which is never a bad thing at all. ",
+    Url:"https://www.homesicktexan.com/coca-cola-braised-carnitas/",
+    Image:"https://www.homesicktexan.com/wp-content/uploads/2017/02/Coca-Cola-braised-carnitas-DSC4126-1-1536x1020.jpg.webp",
+    Tags: []string{"kochen","todo"}}
+    doc2 := doc{Date_added:"2025-05-10T11:15", Title:"ich🚗iel : r/ich_iel",
+    Description:"Lustige Memes und unterhaltsame Diskussionen. Tauche ein in die Welt der Memes und lach mit der Community",
+    Url:"https://www.reddit.com/r/ich_iel/comments/1kklh3s/ichiel/",
+    Image:"https://preview.redd.it/oir97h5vla0f1.jpeg?width=320&crop=smart&auto=webp&s=29bc2c71abd093799b896b36a9d94f3272311249",
+    Tags: []string{"todo"}}
+    return []doc{doc1, doc2}
+}
+
+
+func get_data() []test_data {
+    file, err := os.Open("./data/test_data.json")
+    defer file.Close()
+    if err != nil {
+        panic(err)
+    }
+
+    bytes_read := make([]byte, 1024*1024*1) // Up to 1MB of data
+    n, err := file.Read(bytes_read)
+    if err != nil && err != io.EOF {
+        panic(err)
+    }
+    if n == 0 {
+        panic("fail")
+    }
+    data := make([]test_data, 1)
+    json.Unmarshal(bytes_read[0:n], &data)
+    return data
+}
+func set_data(d []test_data) {
+    b,err := json.Marshal(d)
+    // fmt.Println(string(b))
+    if err != nil {
+        panic(err)
+    }
+
+    err = os.WriteFile("./data/test_data.json", b, 0644)
+    if err != nil{
+        panic(err)
+    }
+}
 
 func main() {
 	router := gin.Default()
@@ -45,6 +124,36 @@ func main() {
 	router.GET("/login", auth.Login) // Unnecessary, as requesting a "AuthRequired" resource will initiate login, but potentially convenient
 	router.GET("/callback", auth.AuthCallback)
 	router.GET("/logout", auth.Logout)
+	router.POST("/clicked", func(c *gin.Context) {
+        c.String(http.StatusOK, time.Now().String())
+    })
+	router.POST("/doc-delete", func(c *gin.Context) {
+        id_str := c.PostForm("id")
+        if id_str == "" {
+            c.String(http.StatusBadRequest, fmt.Sprintln("ERROR! Missing ID!"))
+        }
+        fmt.Printf("Got ID: %s\n", id_str)
+        id, err := strconv.Atoi(id_str)
+        if err != nil {
+            c.String(http.StatusBadRequest, fmt.Sprintf("ERROR! Can't parse ID:%s!\n", id_str))
+        }
+
+        test_data_array := get_data()
+        to_drop := -1
+        for i, e := range test_data_array {
+            if e.Num == id {
+                to_drop = i
+                break
+            }
+        }
+        if to_drop == -1 {
+            c.String(http.StatusBadRequest, fmt.Sprintf("ERROR! No such ID:%d!", id))
+        } else {
+            test_data_array = slices.Delete(test_data_array,to_drop,to_drop+1)
+            set_data(test_data_array)
+            c.HTML(http.StatusOK, "base/doc-list.tmpl", test_data_array)
+        }
+    })
 
 	// Allow access to / for unauthenticated users, but authenticated users will be greated by name.
 	router.GET("/", func(c *gin.Context) {
@@ -58,7 +167,15 @@ func main() {
 		// session.Save() // if it has been changed, which it has not
 		// c.String(http.StatusOK, fmt.Sprintf("<h1>Welcome</h1>\nHello, %s.", name))
         // c.HTML(http.StatusOK, "posts/hello.tmpl", gin.H{"name":name,})
-        c.HTML(http.StatusOK, "posts/hello.tmpl", []map[string]string{{"num":"1","key":"a"},{"num":"2","key":"b"}})
+        // c.HTML(http.StatusOK, "posts/hello.tmpl", []map[string]string{{"num":"1","key":"a"},{"num":"2","key":"b"}})
+
+        // test_data_array := []test_data{
+        //     {Num:1,Key:"a"},{Num:2,Key:"b"},{Num:3,Key:"c"},
+        //     {Num:4,Key:"d"},{Num:5,Key:"e"},{Num:6,Key:"f"},
+        //     {Num:7,Key:"g"},{Num:8,Key:"h"},{Num:9,Key:"i"} }
+        test_data_array := get_data()
+        set_data(test_data_array)
+        c.HTML(http.StatusOK, "posts/hello.tmpl", test_data_array)
 	})
 
 	private := router.Group("/private", auth.AuthRequired())
